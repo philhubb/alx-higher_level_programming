ebde3b648c2e96d1f275fb725af822809c28badf
philhubb <feeliponyenobi@gmail.com>
2022-07-03 17:53:41 +0100

    Mandatory task

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3py
 def print_matrix_integer(matrix=[[]]):
-        for eachlist in matrix:
-            if not eachlist:
-                print()
-                for idx, elem in enumerate(eachlist):
-                    print("{:d}".format(elem), end='')
-                    if idx != (len(eachlist) - 1):
-                        print(" ", end='')
-                    else:
-                        print()
+    for eachlist in matrix:
+        if not eachlist:
+            print()
+            for idx, elem in enumerate(eachlist):
+                print("{:d}".format(elem), end='')
+                if idx != (len(eachlist) - 1):
+                    print(" ", end='')
+                else:
+                    print()
