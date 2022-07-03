64ba43339ebe9316a649aad8dab677085942dc78
philhubb <feeliponyenobi@gmail.com>
2022-07-03 02:30:53 +0100

    Python Task 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if idx < 1:
+    if idx < 0:
         return my_list
     for idxs, integer in enumerate(my_list):
         if idxs == idx:
