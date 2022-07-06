e9bc674f1300bbbd0ebabe5a07fd0bc3efe90876
philhubb <feeliponyenobi@gmail.com>
2022-07-06 08:14:27 +0100

    Mandatory tasks

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    suma = 0
+    if len(sys.argv) == 1:
+        print('0')
+    else:
+        for x in range(len(sys.argv)):
+            if x > 0:
+                sums = sums + int(sys.argv[x])
+
+        print('{}'.format(sums))
